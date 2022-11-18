cee8b90e3c11bc60252a0c768798c6746a801b6e
eTc <88005910+callmeetc@users.noreply.github.com>
2022-11-18 14:09:58 +0300

    Create word_length_counter.py
      This is a simulation of how the len function is able to carry out its task of counting the number of letters in a set of strings. this one Returns the number of letters in a word or sentence.

---
+++ simulation_of_the_len_function_python/word_length_counter.py
@@ -0,0 +1,15 @@
+def wordCount(string):
+    count = 0
+    for letter in  string:
+        #eliminating spaces from the count
+        if letter == " ":
+            continue
+        count += 1
+    return count
+
+print("\n\n\t\t\tReturns the number of letters in a word or sentence.")
+while True:
+    test = input("\n\nEnter a word: ")
+    if test == "end":
+        break
+    print(wordCount(test))
